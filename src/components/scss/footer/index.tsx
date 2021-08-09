dd170e44e709ea5da92c54cb7c9e1a523e4b9c28
Fabz <andradefabian@gmail.com>
2021-08-08 22:59:59 -0400

    lafabulosa initia

--- src/components/scss/footer/index.tsx
+++ src/components/scss/footer/index.tsx
@@ -6,24 +6,35 @@ import styles from "./index.module.scss";
 export const Footer: React.FC = () => {
   return (
     <div className={styles.footer}>
-      <a href="https://github.com/pankod" target="_blank">
+      <a href="/img/logo_white_simple.svg" target="_blank">
         <Image
-          src="/icons/pankod-icon.svg"
-          alt="pankod"
-          width="140"
-          height="28"
+          src="/img/logo_white_simple.svg"
+          alt="La Fabulosa"
+          width="300px"
+          height="auto"
         />
       </a>
+
+      <div className={styles.x}>X</div>
       <div className={styles.icons}>
-        <a href="https://github.com/pankod" target="_blank">
+         
+      <a href="tel:+56930965485" target="_blank">
           <Image
-            src="/icons/github-icon.svg"
-            alt="github"
+            src="/icons/whatsapp-icon.svg"
+            alt="whatsapp"
+            width="28"
+            height="28"
+          />
+        </a>
+        <a href="https://www.instagram.com/_la_fabulosa_" target="_blank"> 
+          <Image
+            src="/icons/instagram-icon.svg"
+            alt="instagram"
             width="28"
             height="29"
           />
         </a>
-        <a href="https://twitter.com/PankodDev" target="_blank">
+        <a href="https://twitter.com/holalafabulosa1" target="_blank">
           <Image
             src="/icons/twitter-icon.svg"
             alt="twitter"
@@ -31,8 +42,19 @@ export const Footer: React.FC = () => {
             height="28"
           />
         </a>
+
+      
+
+         <a href="https://www.linkedin.com/in/la-fabulosa-republica-de-la-montana/" target="_blank"> 
+          <Image
+            src="/icons/linkedin-icon.svg"
+            alt="linkedin"
+            width="28"
+            height="32"
+          />
+        </a>
         <a
-          href="https://www.youtube.com/channel/UCBGOeQkv1XW3ptryLWlQbAQ"
+          href="https://www.youtube.com/c/fabianandrade"
           target="_blank"
         >
           <Image
@@ -43,17 +65,19 @@ export const Footer: React.FC = () => {
           />
         </a>
         <a
-          href="https://www.linkedin.com/company/pankod-yazilim-ve-danismanlik/"
+          href="https://www.facebook.com/la.fabulosa.republica.de.la.montana"
           target="_blank"
         >
           <Image
-            src="/icons/linkedin-icon.svg"
+            src="/icons/facebook-icon.svg"
             alt="linkedin"
             width="28"
             height="32"
           />
         </a>
       </div>
+      <div>
+      <a class={styles.mailto} href="mailto:hola@lafabulosa.org" >hola@lafabulosa.org</a></div>
     </div>
   );
 };
