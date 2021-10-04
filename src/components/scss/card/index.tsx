44398d95f1324da189020a67cf9784f4831e5c98
Fabz <andradefabian@gmail.com>
2021-10-03 22:24:37 -0300

    all good

--- src/components/scss/card/index.tsx
+++ src/components/scss/card/index.tsx
@@ -16,9 +16,10 @@ type CardProps = {
   url: string; 
   children: React.ReactNode;
   hideName: boolean;
+  button: string;
 };
 
-export const Card: React.FC<CardProps> = ({ title, children, url, background, description, poster, cardtype, videoUrl, hideName }) => {
+export const Card: React.FC<CardProps> = ({ title, children, url, background, description, poster, cardtype, videoUrl, hideName, button }) => {
   
   const [showMe, setShowMe] = useState(false);
   function toggle(){
@@ -27,53 +28,45 @@ export const Card: React.FC<CardProps> = ({ title, children, url, background, de
 
   
   return (
-    <div className={styles.card +" "+ cardtype  } style={{backgroundImage: "url("+background+")"}} >
+    <div className={styles.card +" "+ cardtype} style={{backgroundImage: "url("+background+")"}} >
 
       { videoUrl !== "false" ?  
           <div className={styles.container}   >
-             <div className={styles.player}>
-             <ReactPlayer
-               url={videoUrl} 
-               className='react-player'
-               width='100%'
-               height='100%'
-               controls={true}
-        />
-            </div><Link href={url}> 
-             <div className={styles.sideheader}>
+            <div className={styles.player}>
+                <ReactPlayer
+                  url={videoUrl} 
+                  className='react-player'
+                  width='100%'
+                  height='100%'
+                  controls={true}
+                  />
+            </div>
+            <div className={styles.sideheader}>
             
-             
-              {  poster ?   <Image className={styles.poster} src={poster}  layout='fill' alt={title} placeholder="blur" /> : ""}
-              {  !hideName ?  <h2>{title}</h2>: "" }
+              {  poster ?        <Image className={styles.poster} src={poster}  layout='fill' alt={title} placeholder="blur" /> : ""}
+              {  !hideName ?     <h2>{title}</h2>: "" }
               {  description ?   <p>{description}</p>: "" }
+              {  button ?        <div><Link href={url}> <div>{button}</div> </Link></div>: "" }
+            </div>
              
-              </div>
-              </Link>
-            </div> :
+          </div> :
             
-           <div onClick={toggle} style={{
+          <div onClick={toggle} style={{
             transform: showMe?"translateY(-100%)":"translateY(0%)"
           }} className={styles.containercover}>
 
               <div className={styles.coverimage} >
-            <Image className={styles.image} src={poster}  alt={title} placeholder="blur" width="1920px" height="1080px" /> }
-                
-                <div className={styles.header}>{title}</div>
-                <p>{description}</p>
-                <div>{children}</div>
+                <Image className={styles.image} src={poster}  alt={title} placeholder="blur" layout='fill' /> 
               </div>
               <div className={styles.content}>
-              
+                <div className={styles.header}>{title}</div>
+                <p>{description}</p>
               </div>          
             </div>
 
 
       }
-        {/* { url !== "false" ?     
-              <Link href="/blog/hello-world">
-                     <a className={styles.link }>{url}</a>
-              </Link> : ""
-            } */}
+     
     </div>
   );
 };
